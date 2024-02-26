43d8327343982d90ee54de678094da68a023f9d9
unknown <holupkern@gmail.com>
2024-02-26 15:30:24 +0800

    added handlebars helpers

--- 03-express-inheritance/index.js
+++ 03-express-inheritance/index.js
@@ -11,8 +11,33 @@ app.set("view engine", "hbs");
 waxOn.on(hbs.handlebars); //apply wax-on to handlebars in hbs object
 waxOn.setLayoutPath("./views/layouts"); // where to find the template
 
+// name affects what is the name to call it in the hbs file (first param)
+// second param - function called when use helper
+// google handlebars helpers - prewritten
+// write own custom helper
+
+hbs.handlebars.registerHelper("ifEquals", function (arg1, arg2, options){
+        if (arg1 == arg2){
+            return options.fn(this);
+        } else {
+            return options.inverse(this)
+        }
+    }
+); 
+
+
 app.get("/", function(req,res){
     res.render("index");
+})
+
+app.get("/fruits", function(req,res){
+    const fruits = ["apples", "orange", "papaya"]
+    const favorite = "oranges"
+    res.render("fruits", {
+        "fruits": fruits,
+        "favorite" : favorite,
+        "isRaining": false
+    })
 })
 
 app.get("/about-us", function(req,res){
